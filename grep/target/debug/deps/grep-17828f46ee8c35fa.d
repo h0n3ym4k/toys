293438615f42bd25c6cea76ec9d174031f0d1539
h0n3ym4k <98036119lmak@gmail.com>
2022-06-13 17:26:45 +0800

    FUNDING.yml

---
+++ grep/target/debug/deps/grep-17828f46ee8c35fa.d
@@ -0,0 +1,5 @@
+/home/user/Repo/toys/grep/target/debug/deps/grep-17828f46ee8c35fa.rmeta: src/main.rs
+
+/home/user/Repo/toys/grep/target/debug/deps/grep-17828f46ee8c35fa.d: src/main.rs
+
+src/main.rs:
